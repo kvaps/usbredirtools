a8ceecee5021602ab7b2ebfeecedd65812414a1b
kvaps <kvapss@gmail.com>
2015-09-11 15:08:35 +0300

    Resolve https://bugzilla.redhat.com/show_bug.cgi?id=994891

--- usbreconnect.sh
+++ usbreconnect.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+vmid=80230
+usbaddr=192.168.100.95
+usbport=4000
+numberfile=/tmp/usbredirunmber.txt
+
+number=$(cat $numberfile)
+if ! [[ $number =~ ^-?[0-9]+$ ]] ; then
+    echo 0 > $numberfile
+    number=0
+fi
+newnumber=$[$number+1]
+echo $newnumber > $numberfile
+
 expect << EOF
-spawn /usr/sbin/qm monitor 80230
-send "device_del usbredirdev1\r";
-send "chardev-remove usbredirchardev1\r";
-send "chardev-add socket,id=usbredirchardev1,port=4000,host=192.168.100.95\r";
-send "device_add usb-redir,chardev=usbredirchardev1,id=usbredirdev1,bus=ehci.0,debug=4\r";
+spawn /usr/sbin/qm monitor ${vmid}
+send "device_del usbredirdev${number}\r";
+send "chardev-remove usbredirchardev${number}\r";
+send "chardev-add socket,id=usbredirchardev${newnumber},port=${usbport},host=${usbaddr}\r";
+send "device_add usb-redir,chardev=usbredirchardev${newnumber},id=usbredirdev${newnumber},bus=ehci.0,debug=4\r";
 send "quit\r"; expect eof
 EOF
