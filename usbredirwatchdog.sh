6d1f044f03ef17ca0e9534c4cde5c1db29949521
kvaps <kvapss@gmail.com>
2016-08-05 16:23:07 +0300

    Increase CHARDEV and DEVICE

--- usbredirwatchdog.sh
+++ usbredirwatchdog.sh
@@ -40,8 +40,30 @@ for i in ${chardevs[@]}; do
     if [ "$CHARDEV_STATUS" == "disconnected" ]; then
         DEVICE=$(ps aux | grep "$CHARDEV" | grep -oP '(?<=\-device )[^ ]*'$CHARDEV_ID'[^ ]*')
         DEVICE_ID=$(echo "$DEVICE" | grep -oP '(?<=id=)[^,]*')
+
+        # Remove usb device
         qm_monitor "$VM" "device_del $DEVICE_ID"
-        qm_monitor "$VM" "chardev-add $CHARDEV"
+
+        # Create chardev and save output
+        CHARDEV_ADD_OUTPUT="(
+            qm_monitor "$VM" "chardev-add $CHARDEV"
+        )"
+
+        # Check if chardev-add operation contains duplucate error
+        if $(echo $CHARDEV_ADD_OUTPUT | grep -q "Duplicate ID"); then
+            # Increase CHARDEV and DEVICE
+            local CHARDEV_ID_NAME=$(echo "$CHARDEV_ID" | sed 's/[0-9]*$//')
+            local CHARDEV_ID_NUM=$(echo "$CHARDEV_ID" | grep -oP '[0-9]*$')
+            CHARDEV=$(echo "$CHARDEV" | sed "s|id=${CHARDEV_ID_NAME}${CHARDEV_ID_NUM}|id=${CHARDEV_ID_NAME}$((CHARDEV_ID_NUM+1))|g")
+            local DEVICE_ID_NAME=$(echo "$DEVICE_ID" | sed 's/[0-9]*$//')
+            local DEVICE_ID_NUM=$(echo "$DEVICE_ID" | grep -oP '[0-9]*$')
+            DEVICE=$(echo "$DEVICE" | sed -r -e "s|chardev=${CHARDEV_ID_NAME}${CHARDEV_ID_NUM}|chardev=${CHARDEV_ID_NAME}$((CHARDEV_ID_NUM+1))|g" \
+                                             -e "s|id=${DEVICE_ID_NAME}${DEVICE_ID_NUM}|id=${DEVICE_ID_NAME}$((DEVICE_ID_NUM+1))|g"
+            )
+        fi
+
+        # Add usb device
         qm_monitor "$VM" "device_add $DEVICE"
+
     fi 
 done
