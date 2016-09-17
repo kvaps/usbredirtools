5885bb57b6b13937deed01df8ec077f0ba247ac7
kvaps <kvapss@gmail.com>
2016-09-17 17:46:33 +0300

    add: multiple host support

--- usbredir_hook.sh
+++ usbredir_hook.sh
@@ -2,8 +2,9 @@
 ACTION=$1
 VM=one-$2
 TEMPLATE=$3
+HOSTNAME=($(echo $TEMPLATE | base64 -d  | grep -oP '(?<=<HOSTNAME>).*(?=</HOSTNAME>)' | tail -n1))
 USBREDIRS=($(echo $TEMPLATE | base64 -d | grep -oP 'USBREDIR[0-9]+><!\[CDATA\[[a-zA-Z0-9_:.-]*' | grep -oP '[a-zA-Z0-9_:.-]*$'))
-LASTNUM=$(virsh --connect=qemu:///system qemu-monitor-command --hmp $VM 'info chardev' | grep -Po -m1 '(?<=charredir)[0-9]*')
+LASTNUM=$(virsh --connect=qemu+ssh://$HOSTNAME/system qemu-monitor-command --hmp $VM 'info chardev' | grep -Po -m1 '(?<=charredir)[0-9]*')
 
 for USBREDIR in ${USBREDIRS[@]}; do
     case $ACTION in
@@ -14,15 +15,15 @@ for USBREDIR in ${USBREDIRS[@]}; do
             CHARDEV="socket,id=charredir${NUM},host=${CHARDEV_HOST},port=${CHARDEV_PORT}"
             DEVICE="usb-redir,chardev=charredir${NUM},id=redir${NUM},bus=usb.0"
 
-            virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "chardev-add $CHARDEV"
-            virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "device_add $DEVICE"
+            virsh --connect=qemu+ssh://$HOSTNAME/system qemu-monitor-command --hmp $VM "chardev-add $CHARDEV" 
+            virsh --connect=qemu+ssh://$HOSTNAME/system qemu-monitor-command --hmp $VM "device_add $DEVICE"
 
             LASTNUM=$NUM
         ;;
         disconnect)
-            NUM=$(virsh --connect=qemu:///system qemu-monitor-command --hmp $VM 'info chardev' | grep $USBREDIR | grep -Po -m1 '(?<=charredir)[0-9]*')
+            NUM=$(virsh --connect=qemu+ssh://$HOSTNAME/system qemu-monitor-command --hmp $VM 'info chardev' | grep $USBREDIR | grep -Po -m1 '(?<=charredir)[0-9]*')
 
-            virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "device_del redir${NUM}"
+            virsh --connect=qemu+ssh://$HOSTNAME/system qemu-monitor-command --hmp $VM "device_del redir${NUM}"
         ;;
     esac
 done
