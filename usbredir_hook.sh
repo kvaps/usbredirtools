6aa2f17017582397de3987ab78625a7b4033f33f
kvaps <kvapss@gmail.com>
2016-10-12 18:51:04 +0300

    usbredir_hook.sh

--- usbredir_hook.sh
+++ usbredir_hook.sh
@@ -1,3 +1,4 @@
+usbredir_hook.sh
 #!/bin/bash
 #
 # VM_HOOK = [
@@ -27,28 +28,37 @@ VM=one-$2
 TEMPLATE=$3
 HOSTNAME=($(echo $TEMPLATE | base64 -d  | grep -oP '(?<=<HOSTNAME>).*(?=</HOSTNAME>)' | tail -n1))
 USBREDIRS=($(echo $TEMPLATE | base64 -d | grep -oP 'USBREDIR[0-9]+><!\[CDATA\[[a-zA-Z0-9_:.-]*' | grep -oP '[a-zA-Z0-9_:.-]*$'))
-ssh $HOSTNAME <<EOT
+
+case $ACTION in
+    connect)
+
+        ssh $HOSTNAME <<EOT
 LASTNUM=\$(virsh --connect=qemu:///system qemu-monitor-command --hmp $VM 'info chardev' | tr -d $'\\r' | grep -Po -m1 '(?<=charredir)[0-9]*')
 
 for USBREDIR in ${USBREDIRS[@]}; do
-    case $ACTION in
-        connect)
-            NUM=\$((\$LASTNUM+1))
-            CHARDEV_HOST=\$(echo \$USBREDIR | grep -Po '^[^:]+')
-            CHARDEV_PORT=\$(echo \$USBREDIR | grep -Po '[^:]+$')
-            CHARDEV="socket,id=charredir\${NUM},host=\${CHARDEV_HOST},port=\${CHARDEV_PORT}"
-            DEVICE="usb-redir,chardev=charredir\${NUM},id=redir\${NUM},bus=usb.0"
+     NUM=\$((\$LASTNUM+1))
+     CHARDEV_HOST=\$(echo \$USBREDIR | grep -Po '^[^:]+')
+     CHARDEV_PORT=\$(echo \$USBREDIR | grep -Po '[^:]+$')
+     CHARDEV="socket,id=charredir\${NUM},host=\${CHARDEV_HOST},port=\${CHARDEV_PORT}"
+     DEVICE="usb-redir,chardev=charredir\${NUM},id=redir\${NUM},bus=usb.0"
 
-            virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "chardev-add \$CHARDEV" 
-            virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "device_add \$DEVICE"
+     virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "chardev-add \$CHARDEV" 
+     virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "device_add \$DEVICE"
 
-            LASTNUM=\$NUM
-        ;;
-        disconnect)
-
-            NUM=\$(virsh --connect=qemu:///system qemu-monitor-command --hmp $VM 'info chardev' | grep \$USBREDIR | tr -d $'\\r' | grep -Po -m1 '(?<=charredir)[0-9]*')
-            virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "device_del redir\${NUM}"
-        ;;
-    esac
+     LASTNUM=\$NUM
 done
 EOT
+
+    ;;
+    disconnect)
+
+       ssh $HOSTNAME <<EOT
+LASTNUM=\$(virsh --connect=qemu:///system qemu-monitor-command --hmp $VM 'info chardev' | tr -d $'\\r' | grep -Po -m1 '(?<=charredir)[0-9]*')
+
+for USBREDIR in ${USBREDIRS[@]}; do
+    NUM=\$(virsh --connect=qemu:///system qemu-monitor-command --hmp $VM 'info chardev' | grep \$USBREDIR | tr -d $'\\r' | grep -Po -m1 '(?<=charredir)[0-9]*')
+    virsh --connect=qemu:///system qemu-monitor-command --hmp $VM "device_del redir\${NUM}"
+done
+EOT
+    ;;
+esac
