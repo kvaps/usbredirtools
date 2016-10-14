32103060571548d143432eb035615b2369032049
kvaps <kvapss@gmail.com>
2016-10-15 01:45:34 +0300

    upd: logging enchance

--- usbredirserver.sh
+++ usbredirserver.sh
@@ -6,22 +6,17 @@ services() {
     echo $(ls -1 $DATADIR | grep -P '^[0-9]*' | sed -e 's/^/usbredirserver@/' -e 's/$/.service/')
 }
 start() {
-    echo Starting services:
-    IFS=' ' services
-
+    IFS=' ' services | sed 's/^/Starting /'
     systemctl start `services`
 }
 stop() {
-    echo
-    echo Stoping services:
-    IFS=' ' services
-
+    IFS=' ' services | sed 's/^/Stoping /'
     systemctl stop `services`
 }
 watch() {
     local PORT=$(inotifywait -e modify $DATADIR 2> /dev/null | grep -oP '(?<=MODIFY )[0-9]*')
     echo
-    echo Restarting: usbredirserver@$PORT.service 
+    echo Restarting usbredirserver@$PORT.service
     systemctl restart usbredirserver@$PORT.service
     watch
 }
